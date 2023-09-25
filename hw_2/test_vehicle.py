2d999de752fd4d647b5941f4d8824164085711d0
Alevtin <alevtin_antonov@mail.ru>
2023-09-25 17:44:03 +0300

    hw_2 added

--- hw_2/test_vehicle.py
+++ hw_2/test_vehicle.py
@@ -10,3 +10,47 @@
 #  - Проверить, что в режиме парковки (сначала testDrive, потом park, т.е. эмуляция движения транспорта) мотоцикл
 # останавливается (speed = 0).
 
+import unittest
+
+from vehicle import *
+
+
+class TestVehicle(unittest.TestCase):
+    def setUp(self) -> None:
+        self.car = Car('Audi', 'Q5', 2021)
+        self.motorcycle = Motorcycle('BMW', 'S 1000 XR (K69)', 2020)
+
+    def tearDown(self) -> None:
+        self.car = None
+        self.motorcycle = None
+
+    def test_car_is_instance_of_vehicle(self):
+        self.assertTrue(isinstance(self.car, Vehicle))
+
+    def test_car_has_four_wheels(self):
+        self.assertEqual(self.car.num_wheels, 4)
+
+    def test_motorcycle_has_two_wheels(self):
+        self.assertEqual(self.motorcycle.num_wheels, 2)
+
+    def test_car_speed_by_mode_test_drive(self):
+        self.car.test_drive()
+        self.assertEqual(self.car.speed, 60)
+
+    def test_motorcycle_speed_by_mode_test_drive(self):
+        self.motorcycle.test_drive()
+        self.assertEqual(self.motorcycle.speed, 75)
+
+    def test_car_park_mode_after_test_drive(self):
+        self.car.test_drive()
+        self.car.park()
+        self.assertEqual(self.car.speed, 0)
+
+    def test_motorcycle_park_mode_after_test_drive(self):
+        self.motorcycle.test_drive()
+        self.motorcycle.park()
+        self.assertEqual(self.motorcycle.speed, 0)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
--- hw_2/vehicle.py
+++ hw_2/vehicle.py
@@ -1,11 +1,77 @@
-# В этом проекте, вы будете работать с проектом "Vehicle", который представляет собой иерархию классов,
-# включающую абстрактный базовый класс "Vehicle" и два его подкласса "Car" и "Motorcycle".
-# Базовый класс "Vehicle" содержит абстрактные методы "testDrive()" и "park()", а также поля "company",
-# "model", "yearRelease", "numWheels" и "speed".
-# Класс "Car" расширяет "Vehicle" и реализует его абстрактные методы. При создании объекта "Car", число
-# колес устанавливается в 4, а скорость в 0. В методе "testDrive()" скорость устанавливается на 60, а в методе
-# "park()" - обратно в 0.
-# Класс "Motorcycle" также расширяет "Vehicle" и реализует его абстрактные методы. При создании объекта
-# "Motorcycle", число колес устанавливается в 2, а скорость в 0. В методе "testDrive()" скорость
-# устанавливается на 75, а в методе "park()" - обратно в 0.
+from abc import ABCMeta, abstractmethod
 
+
+class Vehicle(metaclass=ABCMeta):
+    def __init__(self, company: str,
+                 model: str,
+                 year_release: int,
+                 num_wheels: int,
+                 speed: int):
+        self._company = company
+        self._model = model
+        self._year_release = year_release
+        self._num_wheels = num_wheels
+        self._speed = speed
+
+    @abstractmethod
+    def test_drive(self) -> None:
+        pass
+
+    @abstractmethod
+    def park(self) -> None:
+        pass
+
+    @property
+    def company(self):
+        return self._company
+
+    @property
+    def model(self):
+        return self._model
+
+    @property
+    def year_release(self):
+        return self._year_release
+
+    @property
+    def num_wheels(self):
+        return self._num_wheels
+
+    @property
+    def speed(self):
+        return self._speed
+
+
+class Car(Vehicle):
+    def __init__(self, company: str, model: str, year_release: int):
+        super().__init__(company, model, year_release, num_wheels=4, speed=0)
+
+    def test_drive(self) -> None:
+        self._speed = 60
+
+    def park(self) -> None:
+        self._speed = 0
+
+    def __repr__(self):
+        return f'Car("{self._company}", "{self._model}", {self._year_release})'
+
+
+class Motorcycle(Vehicle):
+    def __init__(self, company: str, model: str, year_release: int):
+        super().__init__(company, model, year_release, num_wheels=2, speed=0)
+
+    def test_drive(self) -> None:
+        self._speed = 75
+
+    def park(self) -> None:
+        self._speed = 0
+
+    def __repr__(self):
+        return f'Motorcycle("{self._company}", "{self._model}", {self._year_release})'
+
+
+if __name__ == '__main__':
+    auto_1 = Car('Audi', 'Q7', 2020)
+    print(auto_1)
+    moto_1 = Motorcycle('BMV', 'S 1000 XR (K69)', 2021)
+    print(moto_1)
